1da96979ffea95c02f73f43238d09b3a9fe9f8c0
Imani-ES <imanimuh@buffalo.edu>
2023-01-09 12:58:07 -0500

    Header complete
    css is very annoying lol
    about is next

--- imani-website/src/components/header/Header.jsx
+++ imani-website/src/components/header/Header.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import './header.css'
 
+import CV from '../../assets/Imani_Resume_h.pdf'
+import pro_pic from '../../assets/Pro_pic.jpg'
+
+import {FiLinkedin} from 'react-icons/fi'
+import {FaGithub} from 'react-icons/fa'
+
 const Header = () => {
   return (
     <header>
@@ -8,6 +14,26 @@ const Header = () => {
             <h5>Hello, I'm</h5>
             <h1>Imani Muhammad-Graham</h1>
             <h5 className='text-light'></h5>
+
+            <div className='connect_buttons'>
+              <a href={CV} download className='btn'>Download Resume</a>
+              <a href="#contact" className='btn btn-primary'>Connect</a>
+            </div>
+            
+            <div className='me'>
+              <a href="#contact">
+                <img src={pro_pic} alt="me" />
+              </a>
+              
+            </div>
+
+            <a href="#contact" className='scroll__down'>Contact Me!</a>
+
+            <div className='header_socials'>
+              <a href="https://www.linkedin.com/in/imani-muhammad-graham-424b4a127/" target='_blank'><FiLinkedin/></a>
+              <a href="https://github.com" target='_blank'><FaGithub/></a>        
+          </div>
+
         </div>
     </header>    
   )
