3f79ce2ba68257ca0dbebc16732f5012df5d6f57
Imani-ES <imanimuh@buffalo.edu>
2023-01-10 13:06:05 -0500

    about me
    responsive about section done
    need to fix experience overlap in small media devices
    start experience next

--- imani-website/src/components/about/about.jsx
+++ imani-website/src/components/about/about.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
 import './about.css'
+import suit_pic from '../../assets/Suit_w_blue_tie.jpg'
+import {GiDiploma} from 'react-icons/gi'
+import {AiOutlineFolderOpen} from 'react-icons/ai'
+import {RiGameLine} from 'react-icons/ri'
 
 const About = () => {
   return (
     <section id="about">
-      about
+      <h5>A bit</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-img'>
+            <img src={suit_pic} alt="About me Image" />
+          </div>
+        </div>
+
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about_card'>
+              <GiDiploma className='about_icon'/>
+              <h5>Education</h5>
+              <small>
+                University At Buffalo
+              </small>
+            </article>
+          
+            <article className='about_card'>
+              <RiGameLine className='about_icon'/>
+              <h5>For Fun</h5>
+              <small>Blah blha blahcsecefsf</small>
+            </article>
+          
+            <article className='about_card'>
+              <AiOutlineFolderOpen className='about_icon'/>
+              <h5>Projects</h5>
+              <small>IoT interests</small>
+            </article>
+          </div>
+          <div className='about_paragraph'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias facere praesentium beatae pariatur, sit dignissimos natus nemo eaque hic at quos deserunt odit laborum? Aspernatur maxime reprehenderit aperiam placeat culpa!
+            </p>
+            <a href="#contact" className='btn btn-primary'>Lets Talk!</a>
+          </div>
+          
+
+        </div>
+      </div>
     </section>
   )
 }
--- imani-website/src/components/nav/Nav.jsx
+++ imani-website/src/components/nav/Nav.jsx
@@ -7,18 +7,19 @@ import {MdBuildCircle} from 'react-icons/md'
 import {GiBookshelf} from 'react-icons/gi'
 import { useState } from 'react'
 
-const nav = () => {
+const Nav = () => {
   // Implement useState hook to hav nav bar update based on current view
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#header"><FaHome/></a>
-      <a href="#about"><FcAbout/></a>
-      <a href="#exp"><MdBuildCircle/></a>
-      <a href="#portfolio"><GiBookshelf/></a>
-      <a href="#contact"><AiFillMessage/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className = {activeNav ==='#' ? 'active' : ''}><FaHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className = {activeNav ==='#about' ? 'active' : ''}><FcAbout/></a>
+      <a href="#exp" onClick={() => setActiveNav('#exp')} className = {activeNav ==='#about' ? 'exp' : ''}><MdBuildCircle/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className = {activeNav ==='#portfolio' ? 'active' : ''}><GiBookshelf/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className = {activeNav ==='#contact' ? 'active' : ''}><AiFillMessage/></a>
       
     </nav>
   )
 }
 
-export default nav
+export default Nav
