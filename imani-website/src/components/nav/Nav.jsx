cc63684b5983e289dad7e862b3b5ed9c213c9f4f
Imani-ES <imanimuh@buffalo.edu>
2023-01-09 22:08:57 -0500

    implement use state next

--- imani-website/src/components/nav/Nav.jsx
+++ imani-website/src/components/nav/Nav.jsx
@@ -5,7 +5,10 @@ import {FcAbout} from 'react-icons/fc'
 import {AiFillMessage} from 'react-icons/ai'
 import {MdBuildCircle} from 'react-icons/md'
 import {GiBookshelf} from 'react-icons/gi'
+import { useState } from 'react'
+
 const nav = () => {
+  // Implement useState hook to hav nav bar update based on current view
   return (
     <nav>
       <a href="#header"><FaHome/></a>
