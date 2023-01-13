e54ef01d76707fd11c35e3d1f19f054a79d5605f
Imani-ES <imanimuh@buffalo.edu>
2023-01-13 13:51:10 -0500

    start contacts next

--- imani-website/src/components/contact/contact.jsx
+++ imani-website/src/components/contact/contact.jsx
@@ -4,7 +4,18 @@ import './contact.css'
 const Contact = () => {
   return (
     <section id = "contact">
-      contact
+      <h2>Get In Touch</h2>
+      <h5>Contact Me</h5>
+
+      <div className='container contact__container'>
+        <div className="contact_options">
+          <article className="contact_option">
+            
+          </article>
+        </div>
+        
+        <form action=""></form>
+      </div>
     </section>
   )
 }
