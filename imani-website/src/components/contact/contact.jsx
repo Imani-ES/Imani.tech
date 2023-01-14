9f65e385b01e7a832b84f7f37669ad506f3b77e2
Imani-ES <imanimuh@buffalo.edu>
2023-01-14 13:49:09 -0500

    contact complete
    Responsive contact section complete.
    Need to set up form message handling next.

--- imani-website/src/components/contact/contact.jsx
+++ imani-website/src/components/contact/contact.jsx
@@ -1,20 +1,40 @@
 import React from 'react'
 import './contact.css'
+import {SiMinutemailer} from 'react-icons/si'
+
+function copy_name_ToClipboard() {
+  navigator.clipboard.writeText('Imani Muhammad-Graham');
+  alert("You copied my Linkedin Username!");
+};
 
 const Contact = () => {
   return (
     <section id = "contact">
-      <h2>Get In Touch</h2>
-      <h5>Contact Me</h5>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
 
       <div className='container contact__container'>
         <div className="contact_options">
           <article className="contact_option">
-            
+            <SiMinutemailer className='contact_icon'/>
+            <h2>Email</h2>
+            <h5>imanim454@gmail.com</h5>
+            <a href="mailto:imanim454@gmail.com">Send a Message</a>
+          </article>
+          <article className="contact_option" onClick={copy_name_ToClipboard}>
+            <SiMinutemailer className='contact_icon'/>
+            <h2>Linkedin</h2>
+            <h5 >Imani Muhammad-Graham</h5>
+            <a href="https://www.linkedin.com/in/imani-muhammad-graham-424b4a127/">Send a Message</a>
           </article>
         </div>
         
-        <form action=""></form>
+        <form className='contact_form' action="">
+          <input type="text" name='name' placeholder='Your full name' required />
+          <input type="text" name='email' placeholder='Your email' required />
+          <textarea name="message" placeholder='Type your message here'  required></textarea>
+          <button className='btn btn-primary' type='submit'>Send Message</button>
+        </form>
       </div>
     </section>
   )
