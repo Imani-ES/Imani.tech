c525dceab6732bffe3dfc3984c6912cdd14fe822
Imani-ES <imanimuh@buffalo.edu>
2023-01-16 20:09:24 -0500

    done with footer
    pretty much done, just need to deploy

--- imani-website/src/components/footer/Footer.jsx
+++ imani-website/src/components/footer/Footer.jsx
@@ -6,14 +6,14 @@ import {BsInstagram} from 'react-icons/bs'
 const Footer = () => {
   return (
     <footer>
-      <a href="#">Imani Muhammad-Graham</a>
+      <a href="#" className='footer_logo'>Imani Muhammad-Graham</a>
 
       <ul className='permalinks'>
-        <a href="#">Home</a>
-        <a href="#about">About</a>
-        <a href="#exp">Experience</a>
-        <a href="#portfolio">Portfolio</a>
-        <a href="#contact">Contact</a>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#exp">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
 
       <div className="footer_socials">
