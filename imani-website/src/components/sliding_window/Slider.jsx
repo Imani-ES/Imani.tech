5a3c416eec88d067da00b9773ea2b914544eb708
Imani-ES <imanimuh@buffalo.edu>
2023-02-03 12:14:04 -0500

    attempted slilder

--- imani-website/src/components/sliding_window/Slider.jsx
+++ imani-website/src/components/sliding_window/Slider.jsx
@@ -2,8 +2,45 @@ import React from 'react'
 import './slider.css'
 import {AiOutlineDoubleLeft} from 'react-icons/ai'
 import {AiOutlineDoubleRight} from 'react-icons/ai'
+import bored_nerds from '../../assets/Bored_Nerds.jpg'
 
-
+const data = [
+  {
+    id: 1,
+    image:  bored_nerds,
+    title:  'Bored Nerds',
+    gh: "https://github.com/Imani-ES/bored-nerds",
+    demo: "https://github.com/Imani-ES/bored-nerds"
+  },
+  {
+    id: 2,
+    image:  bored_nerds,
+    title:  'Bored Nerds',
+    gh: "https://github.com/Imani-ES/bored-nerds",
+    demo: "https://github.com/Imani-ES/bored-nerds"
+  },
+  {
+    id: 3,
+    image:  bored_nerds,
+    title:  'Bored Nerds',
+    gh: "https://github.com/Imani-ES/bored-nerds",
+    demo: "https://github.com/Imani-ES/bored-nerds"
+  },
+  {
+    id: 4,
+    image:  bored_nerds,
+    title:  'Bored Nerds',
+    gh: "https://github.com/Imani-ES/bored-nerds",
+    demo: "https://github.com/Imani-ES/bored-nerds"
+  },
+  {
+    id: 5,
+    image:  bored_nerds,
+    title:  'Bored Nerds',
+    gh: "https://github.com/Imani-ES/bored-nerds",
+    demo: "https://github.com/Imani-ES/bored-nerds"
+  }
+];
 const slider = () => {
   return (
     <div className='slider__container'>
@@ -12,8 +49,23 @@ const slider = () => {
             <AiOutlineDoubleLeft/>
         </div>
 
-        <div className="slide_content">
-
+        <div className="slider_content">
+        {
+          data.map(({id, image, title, gh, demo}) => {
+            return(
+              <article key = {id} className='slider_item'>
+                <div className='slider_item_img'>
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className='slider'>
+                  <a href={gh} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Demo</a>
+                </div>
+              </article>
+            )          
+          })
+        }
         </div>
 
         <div className="right_slide slide_arrow">
